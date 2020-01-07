1b418b1acf641aa1d50a6ce8cf65de127b639f35
Ahnder <skguss1@gmail.com>
2020-01-07 20:35:11 +0900

    create Post-User Relationship

--- models/Post.js
+++ models/Post.js
@@ -8,6 +8,7 @@ const util = require('../util');
 let postSchema = mongoose.Schema({
     title: { type: String, require: true, },
     body: { type: String, },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true, },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, },
 }, {toObject: { virtuals: true, }}); // virtual들을 object에서 보여주는 mongoose schema의 option
--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,10 @@ const util = require('../util');
 // Index
 router.get('/', (req, res) => {
     Post.find({})
+        // Model.populate() 함수는 relationship이 형성되어 있는 항목의 값을 생성해준다
+        // 현재 post의 author에는 user의 id가 기록되어 있는데,
+        // 이 값을 바탕으로 실제 user의 값을 author에 생성하게 된다.
+        .populate('author')
         .sort("-createdAt")  // 나중에 생성된 데이터가 위로 오도록 정렬 
         .exec((err, posts) => {
             if(err) return res.json(err);
@@ -24,6 +28,8 @@ router.get('/new', (req, res) => {
 
 // Create
 router.post('/', (req, res) => {
+    // 글을 작성할떄는 req.user._id를 가져와서 post의 author에 기록한다.
+    req.body.author = req.user._id;
     Post.create(req.body, (err, post) => {
         if (err) {
             req.flash('post', req.body);
@@ -36,10 +42,12 @@ router.post('/', (req, res) => {
 
 // Show
 router.get('/:id', (req, res) => {
-    Post.findOne({_id:req.params.id}, (err, post) => {
-        if (err) return res.json(err);
-        res.render('posts/show', {post:post});
-    });
+    Post.findOne({_id:req.params.id})
+        .populate('author')
+        .exec((err, post) => {
+            if (err) return res.json(err);
+            res.render('posts/show', {post:post});
+        });
 });
 
 // Edit
